c814ec5199c71220d8b16bfc4026b4d6cf45354e
XilunWu <wu636@purdue.edu>
2015-09-30 17:06:03 -0400

    part2

--- proj1/p2_wu636.sql
+++ proj1/p2_wu636.sql
@@ -17,24 +17,26 @@ BEGIN
 	dbms_output.put_line('-------------------------------------------------------------------------------------');
 	OPEN c_univ;
 	LOOP
+		BEGIN
 		FETCH c_univ into c_univid, c_univname;
 		EXIT WHEN c_univ%notfound;
-		BEGIN
+		--BEGIN
 		select count(*) into NumberOfEmployees from Employee, Graduate
 		where Graduate.EmpId = Employee.EmpId and Graduate.UnivId = c_univid;
+		/*
 		EXCEPTION 
         WHEN NO_DATA_FOUND THEN
           	dbms_output.put_line('No data found');
             NumberOfEmployees := 0;
 		END;
-
+		*/
 		select sum(table1.num) into NumberOfProjects from (
 			select ep.EmpId, count(distinct ep.ProjId) as num
 			from EmpProject ep, Graduate g, University u
 			where u.UnivId = c_univid and u.UnivId = g.UnivId and g.EmpId = ep.EmpId
 			group by ep.EmpId
 		) table1;
-
+		/*
 		BEGIN
 			select count(distinct ep.ProjId) into tmp
 			from EmpProject ep, Graduate g, University u
@@ -45,6 +47,7 @@ BEGIN
         		dbms_output.put_line('No data found');
            	 	NumberOfProjects := 0;
 		END;
+		*/
 
 		IF NumberOfEmployees = 0 THEN
 			AvgProjects := 0;
@@ -52,15 +55,17 @@ BEGIN
 			AvgProjects := NumberOfProjects / NumberOfEmployees;
 		END IF;
 		
-		BEGIN
+		--BEGIN
 		select count(distinct ProjectManager.MgrId) into NumberOfManagers from ProjectManager, Graduate
 		where Graduate.EmpId = ProjectManager.MgrId and Graduate.UnivId = c_univid;
+		/*
 		EXCEPTION 
         	WHEN NO_DATA_FOUND THEN
        	    NumberOfManagers := 0;
 		END;
-
+		*/
 		dbms_output.put_line(c_univname || chr(9) || chr(9) || NumberOfEmployees || chr(9) || chr(9) || AvgProjects || chr(9) || chr(9) || NumberOfManagers);
+		END;
 	END LOOP;
 	CLOSE c_univ;
 END;
@@ -68,9 +73,8 @@ END;
 show errors;
 execute sp_univ_emp;
 
-select sum(table1.num) from (
-			select ep.EmpId, count(distinct ep.ProjId) as num
-			from EmpProject ep, Graduate g, University u
-			where u.UnivId = 3 and u.UnivId = g.UnivId and g.EmpId = ep.EmpId
-			group by ep.EmpId
-		) table1;
+
+/*
+	#2
+*/
+
