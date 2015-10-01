d46e8019f693da7ce40d22b72e4a4e058a5914ca
Xilun Wu <wu636@purdue.edu>
2015-09-30 22:35:23 -0400

    #3 is left

--- proj1/p2_wu636.sql
+++ proj1/p2_wu636.sql
@@ -1,78 +1,90 @@
 set serveroutput on
+@db.sql
 
 create or replace procedure sp_univ_emp 
-is
+IS
 	c_univid University.UnivId%type;
 	c_univname University.UnivName%type;
 	NumberOfEmployees INTEGER := 0;
-	AvgProjects numeric(2) := 0;
+	AvgProjects FLOAT := 0;
 	NumberOfProjects INTEGER := 0;
 	NumberOfManagers INTEGER := 0;
 	CURSOR c_univ is select * from University;
-	tmp INTEGER;
-BEGIN
-	--dbms_output.put_line('start');
-	dbms_output.put_line('UniversityName  NumberOfEmployees  AvgProjects   NumberOfManagers');
-	dbms_output.put_line('UniversityName' || chr(9) || 'NumberOfEmployees' || chr(9) || 'AvgProjects' || chr(9) || 'NumberOfManagers');
-	dbms_output.put_line('-------------------------------------------------------------------------------------');
-	OPEN c_univ;
-	LOOP
-		BEGIN
-		FETCH c_univ into c_univid, c_univname;
-		EXIT WHEN c_univ%notfound;
-		--BEGIN
-		select count(*) into NumberOfEmployees from Employee, Graduate
-		where Graduate.EmpId = Employee.EmpId and Graduate.UnivId = c_univid;
-		/*
+	
+	procedure numOfEmp (univid IN University.UnivId%type,
+					num OUT INTEGER)
+	IS
+		BEGIN		
+		num := 0;
+			select count(*) into num from Employee, Graduate
+			where Graduate.UnivId = univid and Graduate.EmpId = Employee.EmpId;
 		EXCEPTION 
         WHEN NO_DATA_FOUND THEN
           	dbms_output.put_line('No data found');
-            NumberOfEmployees := 0;
+            num := 0;
 		END;
-		*/
-		select sum(table1.num) into NumberOfProjects from (
-			select ep.EmpId, count(distinct ep.ProjId) as num
-			from EmpProject ep, Graduate g, University u
-			where u.UnivId = c_univid and u.UnivId = g.UnivId and g.EmpId = ep.EmpId
-			group by ep.EmpId
-		) table1;
-		/*
+
+	procedure numOfProj (univid IN University.UnivId%type,
+					num OUT INTEGER)
+	IS
+		tmp INTEGER;
 		BEGIN
-			select count(distinct ep.ProjId) into tmp
-			from EmpProject ep, Graduate g, University u
-			where u.UnivId = c_univid and u.UnivId = g.UnivId and g.EmpId = ep.EmpId
-			group by ep.EmpId;
+			select count(*) into tmp from EmpProject ep, Graduate g
+			where g.UnivId = univid and g.EmpId = ep.EmpId;		
+			IF tmp = 0 THEN
+           		num := 0;
+           	ELSE
+           		num := 1;
+           	END IF;
+		END;
+
+	procedure numOfMgr (univid IN University.UnivId%type,
+					num OUT INTEGER)
+	IS
+		BEGIN
+			select count(distinct ProjectManager.MgrId) into num from ProjectManager, Graduate
+			where Graduate.EmpId = ProjectManager.MgrId and Graduate.UnivId = univid;
 		EXCEPTION 
         	WHEN NO_DATA_FOUND THEN
-        		dbms_output.put_line('No data found');
-           	 	NumberOfProjects := 0;
+       	    num := 0;
 		END;
-		*/
+		
+BEGIN
+	--dbms_output.put_line('start');
+	dbms_output.put_line('UniversityName  NumberOfEmployees  AvgProjects   NumberOfManagers');
+	dbms_output.put_line('-------------------------------------------------------------------------------------');
+	OPEN c_univ;
+	LOOP
+		FETCH c_univ into c_univid, c_univname;
+		EXIT WHEN c_univ%notfound;
+		
+		numOfEmp(c_univid, NumberOfEmployees);
+		numOfProj(c_univid, NumberOfProjects);
+		IF NumberOfProjects = 0 THEN
+			NULL;
+		ELSE
+			select sum(table1.num) into NumberOfProjects from (
+				select ep.EmpId, count(distinct ep.ProjId) as num
+				from EmpProject ep, Graduate g, University u
+				where u.UnivId = c_univid and u.UnivId = g.UnivId and g.EmpId = ep.EmpId
+				group by ep.EmpId
+			) table1;
+		END IF;
 
 		IF NumberOfEmployees = 0 THEN
 			AvgProjects := 0;
 		ELSE
-			AvgProjects := NumberOfProjects / NumberOfEmployees;
+			AvgProjects := (1.0 * NumberOfProjects) / NumberOfEmployees;
 		END IF;
 		
-		--BEGIN
-		select count(distinct ProjectManager.MgrId) into NumberOfManagers from ProjectManager, Graduate
-		where Graduate.EmpId = ProjectManager.MgrId and Graduate.UnivId = c_univid;
-		/*
-		EXCEPTION 
-        	WHEN NO_DATA_FOUND THEN
-       	    NumberOfManagers := 0;
-		END;
-		*/
-		dbms_output.put_line(c_univname || chr(9) || chr(9) || NumberOfEmployees || chr(9) || chr(9) || AvgProjects || chr(9) || chr(9) || NumberOfManagers);
-		END;
+		numOfMgr(c_univid, NumberOfManagers);
+		dbms_output.put_line(c_univname || chr(9) || NumberOfEmployees || chr(9) || AvgProjects || chr(9) || NumberOfManagers);
 	END LOOP;
 	CLOSE c_univ;
 END;
 /
 show errors;
 execute sp_univ_emp;
-
 
 /*
 	#2
@@ -140,3 +152,5 @@ exec sp_emp_mate;
 /*
 	#3
 */
+
+@droptables.sql
